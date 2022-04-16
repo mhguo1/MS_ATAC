28d01f510a8b5c1690e1f700aa8e1a5b87443a8a
mhguo1 <mhguo1@gmail.com>
2022-04-15 23:11:10 -0400

    Update ldsc.sh

--- LDSC/ldsc.sh
+++ LDSC/ldsc.sh
@@ -13,7 +13,7 @@ python /software/ldsc/munge_sumstats.py \
 --N-cas 14802 \
 --N-con 26703
 
-#Step 2: Compute LD scores for ATAC-seq peaks from each cell type
+#Step 2: Compute LD scores for ATAC-seq peaks from each cell type. Example shown below for Buenrostro data, but similar analyses done for Calderon et al. data and MS patient data.
 for cell in B CD4 CD8 CLP CMP Ery GMP HSC LMPP MEP MPP Mega Mono NK mDC pDC_ATAC
 do
 for i in {1..22}
@@ -33,7 +33,7 @@ python /software/ldsc/ldsc.py \
 done
 done
 
-#Step 3: Partitioned heritability for single annotation
+#Step 3: Partitioned heritability for single annotation. Example shown below for Buenrostro data, but similar analyses done for Calderon et al. data and MS patient data.
 for cell in B CD4 CD8 CLP CMP Ery GMP HSC LMPP MEP MPP Mega Mono NK mDC pDC_ATAC
 do
 
@@ -44,5 +44,33 @@ python /cvar/jhlab/mguo/SOFTWARE/ldsc/ldsc.py \
 --w-ld-chr /ldsc_files/weights_hm3_no_hla/weights. \
 --frqfile-chr /ldsc_files/1000G_frq/1000G.mac5eur. \
 --print-coefficients \
---out ${trait}_buenrostro_ATAC
+--out MS_${cell}_buenrostro_ATAC
+done
+
+#Step 4: Partitioned heritability for joint. Example shown below for Buenrostro data, but similar analyses done for Calderon et al. data and MS patient data.
+python /cvar/jhlab/mguo/SOFTWARE/ldsc/ldsc.py \
+--h2 MS.sumstats.gz \
+--overlap-annot \
+--ref-ld-chr /ldsc_files/baseline/baseline.,/ldsc_files/cell_type_groups/buenrostro/B_ATAC.,/ldsc_files/cell_type_groups/buenrostro/CD4_ATAC.,/ldsc_files/cell_type_groups/buenrostro/CD8_ATAC.,/ldsc_files/cell_type_groups/buenrostro/CLP_ATAC.,/ldsc_files/cell_type_groups/buenrostro/CMP_ATAC.,/ldsc_files/cell_type_groups/buenrostro/Ery_ATAC.,/ldsc_files/cell_type_groups/buenrostro/GMP_ATAC.,/ldsc_files/cell_type_groups/buenrostro/HSC_ATAC.,/ldsc_files/cell_type_groups/buenrostro/LMPP_ATAC.,/ldsc_files/cell_type_groups/buenrostro/MEP_ATAC.,/ldsc_files/cell_type_groups/buenrostro/MPP_ATAC.,/ldsc_files/cell_type_groups/buenrostro/Mega_ATAC.,/ldsc_files/cell_type_groups/buenrostro/Mono_ATAC.,/ldsc_files/cell_type_groups/buenrostro/NK_ATAC.,/ldsc_files/cell_type_groups/buenrostro/mDC_ATAC.,/ldsc_files/cell_type_groups/buenrostro/pDC_ATAC. \
+--w-ld-chr /ldsc_files/weights_hm3_no_hla/weights. \
+--frqfile-chr /ldsc_files/1000G_frq/1000G.mac5eur. \
+--print-coefficients \
+--out MS_joint_buenrostro_ATAC
+
+
+#Step 4: Partitioned heritability for pairwise model. Example shown below for Buenrostro data, but similar analyses done for Calderon et al. data and MS patient data.
+for cell1 in B CD4 CD8 CLP CMP Ery GMP HSC LMPP MEP MPP Mega Mono NK mDC pDC_ATAC
+do
+for cell2 in B CD4 CD8 CLP CMP Ery GMP HSC LMPP MEP MPP Mega Mono NK mDC pDC_ATAC
+do
+
+python /cvar/jhlab/mguo/SOFTWARE/ldsc/ldsc.py \
+--h2 MS.sumstats.gz \
+--overlap-annot \
+--ref-ld-chr /ldsc_files/baseline/baseline.,/ldsc_files/cell_type_groups/buenrostro/${cell1}_ATAC.,/ldsc_files/cell_type_groups/buenrostro/${cell2}_ATAC. \
+--w-ld-chr /ldsc_files/weights_hm3_no_hla/weights. \
+--frqfile-chr /ldsc_files/1000G_frq/1000G.mac5eur. \
+--print-coefficients \
+--out MS_${cell1}_${cell2}_buenrostro_ATAC
+done
 done
